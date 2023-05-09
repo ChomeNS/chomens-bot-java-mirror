c97fdb7b1eebe73ea06a46ba6b378031ed89bdde
ChomeNS <95471003+ChomeNS@users.noreply.github.com>
2023-05-09 09:47:37 +0700

    no bad bad badb adb

--- src/main/java/land/chipmunk/chayapak/chomens_bot/Configuration.java
+++ src/main/java/land/chipmunk/chayapak/chomens_bot/Configuration.java
@@ -61,6 +61,8 @@ public class Configuration {
     }
 
     public static class SelfCare {
+        @Getter public int checkInterval;
+
         @Getter public boolean op = true;
         @Getter public boolean gamemode = true;
         @Getter public boolean endCredits = true;
