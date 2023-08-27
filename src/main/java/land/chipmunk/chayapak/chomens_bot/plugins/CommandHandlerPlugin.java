d4dc8ccdbcbdf3d552f5dc0ceec2d9a42372a22a
ChomeNS <95471003+ChomeNS@users.noreply.github.com>
2023-08-27 11:23:05 +0700

    oopsies i forgor

--- src/main/java/land/chipmunk/chayapak/chomens_bot/plugins/CommandHandlerPlugin.java
+++ src/main/java/land/chipmunk/chayapak/chomens_bot/plugins/CommandHandlerPlugin.java
@@ -149,8 +149,7 @@ public class CommandHandlerPlugin {
 
                 if (
                         command.trustLevel == TrustLevel.OWNER &&
-                                !userHash.equals(bot.hashing.getOwnerHash(splitInput[0], context.sender, false)) &&
-                                !userHash.equals(bot.hashing.getOwnerHash(splitInput[0], context.sender, true))
+                                !bot.hashing.isCorrectOwnerHash(userHash, splitInput[0], context.sender)
                 ) return Component.text("Invalid OwnerHash").color(NamedTextColor.RED);
             }
         }
