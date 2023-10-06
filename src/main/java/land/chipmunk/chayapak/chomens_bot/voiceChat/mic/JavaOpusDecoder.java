06ccbd37291a74fa2cab4ba76c2758f85dcd2ffb
ChomeNS <95471003+ChomeNS@users.noreply.github.com>
2023-10-06 17:11:08 +0700

    one step from getting it working (BUT that step will never be done)

---
+++ src/main/java/land/chipmunk/chayapak/chomens_bot/voiceChat/mic/JavaOpusDecoder.java
@@ -0,0 +1,70 @@
+package land.chipmunk.chayapak.chomens_bot.voiceChat.mic;
+
+import org.concentus.OpusDecoder;
+import org.concentus.OpusException;
+
+public class JavaOpusDecoder {
+    protected OpusDecoder opusDecoder;
+    protected short[] buffer;
+    protected int sampleRate;
+    protected int frameSize;
+    protected int maxPayloadSize;
+
+    public JavaOpusDecoder (int sampleRate, int frameSize, int maxPayloadSize) {
+        this.sampleRate = sampleRate;
+        this.frameSize = frameSize;
+        this.maxPayloadSize = maxPayloadSize;
+        this.buffer = new short[4096];
+        open();
+    }
+
+    private void open() {
+        if (opusDecoder != null) {
+            return;
+        }
+        try {
+            opusDecoder = new OpusDecoder(sampleRate, 1);
+        } catch (OpusException e) {
+            throw new IllegalStateException("Opus decoder error " + e.getMessage());
+        }
+    }
+
+    public short[] decode(byte[] data) {
+        if (isClosed()) {
+            throw new IllegalStateException("Decoder is closed");
+        }
+        int result;
+
+        try {
+            if (data == null || data.length == 0) {
+                result = opusDecoder.decode(null, 0, 0, buffer, 0, frameSize, false);
+            } else {
+                result = opusDecoder.decode(data, 0, data.length, buffer, 0, frameSize, false);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to decode audio data: " + e.getMessage());
+        }
+
+        short[] audio = new short[result];
+        System.arraycopy(buffer, 0, audio, 0, result);
+        return audio;
+    }
+
+    public boolean isClosed() {
+        return opusDecoder == null;
+    }
+
+    public void close() {
+        if (opusDecoder == null) {
+            return;
+        }
+        opusDecoder = null;
+    }
+
+    public void resetState() {
+        if (isClosed()) {
+            throw new IllegalStateException("Decoder is closed");
+        }
+        opusDecoder.resetState();
+    }
+}
---
+++ src/main/java/land/chipmunk/chayapak/chomens_bot/voiceChat/mic/JavaOpusEncoder2.java
@@ -0,0 +1,72 @@
+package land.chipmunk.chayapak.chomens_bot.voiceChat.mic;
+
+import org.concentus.OpusApplication;
+import org.concentus.OpusEncoder;
+
+public class JavaOpusEncoder2 {
+    public OpusEncoder opusEncoder;
+    public byte[] buffer;
+    public int sampleRate;
+    public int frameSize;
+    public int maxPayloadSize;
+    public OpusApplication application;
+
+    public JavaOpusEncoder2 (int sampleRate, int frameSize, int maxPayloadSize, OpusApplication application) {
+        this.sampleRate = sampleRate;
+        this.frameSize = frameSize;
+        this.maxPayloadSize = maxPayloadSize;
+        this.application = application;
+        this.buffer = new byte[maxPayloadSize];
+        open();
+    }
+
+    private void open() {
+        if (opusEncoder != null) {
+            return;
+        }
+        try {
+            opusEncoder = new OpusEncoder(sampleRate, 1, application);
+        } catch (Exception e) {
+            throw new IllegalStateException("Opus encoder error " + e.getMessage());
+        }
+    }
+
+    public byte[] encode(short[] rawAudio) {
+        if (isClosed()) {
+            throw new IllegalStateException("Encoder is closed");
+        }
+
+        int result;
+        try {
+            result = opusEncoder.encode(rawAudio, 0, frameSize, buffer, 0, buffer.length);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to encode audio data: " + e.getMessage());
+        }
+
+        if (result < 0) {
+            throw new RuntimeException("Failed to encode audio data");
+        }
+
+        byte[] audio = new byte[result];
+        System.arraycopy(buffer, 0, audio, 0, result);
+        return audio;
+    }
+
+    public void resetState() {
+        if (isClosed()) {
+            throw new IllegalStateException("Encoder is closed");
+        }
+        opusEncoder.resetState();
+    }
+
+    public boolean isClosed() {
+        return opusEncoder == null;
+    }
+
+    public void close() {
+        if (isClosed()) {
+            return;
+        }
+        opusEncoder = null;
+    }
+}
