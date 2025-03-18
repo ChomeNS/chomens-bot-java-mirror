04a8d10e1f2339092e84d0b65ee9f6ca679b37a7
ChomeNS <95471003+ChomeNS@users.noreply.github.com>
2025-03-18 19:05:31 +0700

    feat: use ChronoUnit for cloop (forever cloop now real!!!)

--- src/main/java/me/chayapak1/chomens_bot/plugins/CloopPlugin.java
+++ src/main/java/me/chayapak1/chomens_bot/plugins/CloopPlugin.java
@@ -3,6 +3,7 @@ package me.chayapak1.chomens_bot.plugins;
 import me.chayapak1.chomens_bot.Bot;
 import me.chayapak1.chomens_bot.data.cloop.CommandLoop;
 
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ScheduledFuture;
@@ -18,11 +19,11 @@ public class CloopPlugin {
         this.bot = bot;
     }
 
-    public void add (TimeUnit unit, int interval, String command) {
+    public void add (ChronoUnit unit, int interval, String command) {
         Runnable loopTask = () -> bot.core.run(command);
 
         loops.add(new CommandLoop(command, interval, unit));
-        loopTasks.add(bot.executor.scheduleAtFixedRate(loopTask, 0, interval, unit));
+        loopTasks.add(bot.executor.scheduleAtFixedRate(loopTask, 0, interval, TimeUnit.of(unit)));
     }
 
     public CommandLoop remove (int index) {
