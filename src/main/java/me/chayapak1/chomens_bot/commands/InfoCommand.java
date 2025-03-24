043efe95473955d2af97cf33cc443e3aab4edfdd
ChomeNS <95471003+ChomeNS@users.noreply.github.com>
2025-03-24 17:02:54 +0700

    fix: last commit didn't fix it lol

--- src/main/java/me/chayapak1/chomens_bot/commands/InfoCommand.java
+++ src/main/java/me/chayapak1/chomens_bot/commands/InfoCommand.java
@@ -71,7 +71,7 @@ public class InfoCommand extends Command {
         final Bot bot = context.bot;
 
         final String action = !context.userInputCommandName.equalsIgnoreCase(this.name) ? // if the input command is not `info`
-                context.userInputCommandName : // use that as the action (e.g. "discord", "creator")
+                context.userInputCommandName.toLowerCase() : // use that as the action (e.g. "discord", "creator")
                 context.getString(false, false, true); // else just take the argument of `info`
 
         switch (action) {
