dbd4cdb6563efbf70a9b194bb563a7cbdff1c106
ChomeNS <95471003+ChomeNS@users.noreply.github.com>
2025-04-10 14:08:54 +0700

    refactor: remove invalid comment

--- src/main/java/me/chayapak1/chomens_bot/util/ChatMessageUtilities.java
+++ src/main/java/me/chayapak1/chomens_bot/util/ChatMessageUtilities.java
@@ -9,8 +9,6 @@ import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 
 public class ChatMessageUtilities {
     private static final LegacyComponentSerializer SERIALIZER = LegacyComponentSerializer
-
-            // https://sus.red/abc?a=b&c=d will break the styles, click event, hover events, everything
             .legacySection()
 
             .toBuilder()
