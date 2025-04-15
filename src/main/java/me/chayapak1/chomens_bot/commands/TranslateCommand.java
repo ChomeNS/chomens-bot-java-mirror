5cd1cc897627ec031f54e97c9b713f5e902d2224
ChomeNS <95471003+ChomeNS@users.noreply.github.com>
2025-04-15 08:20:57 +0700

    refactor: change translate API again

--- src/main/java/me/chayapak1/chomens_bot/commands/TranslateCommand.java
+++ src/main/java/me/chayapak1/chomens_bot/commands/TranslateCommand.java
@@ -1,12 +1,12 @@
 package me.chayapak1.chomens_bot.commands;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import me.chayapak1.chomens_bot.Bot;
 import me.chayapak1.chomens_bot.command.Command;
 import me.chayapak1.chomens_bot.command.CommandContext;
 import me.chayapak1.chomens_bot.command.CommandException;
 import me.chayapak1.chomens_bot.command.TrustLevel;
-import me.chayapak1.chomens_bot.util.HTMLUtilities;
-import me.chayapak1.chomens_bot.util.HttpUtilities;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 
@@ -14,15 +14,19 @@ import java.net.URI;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.List;
 
+// stolen from vencord :D
+// https://github.com/Vendicated/Vencord/blob/1fa6181f7e6526e1bf2e85260d9051000916c47c/src/plugins/translate/utils.ts
 public class TranslateCommand extends Command {
-    private static final String TRANSLATE_MOBILE_URL = "https://translate.google.com/m?sl=%s&tl=%s&hl=en&q=%s";
-    private static final Pattern FIND_RESULT_PATTERN = Pattern.compile(
-            "class=\"result-container\">([^<]*)</div>",
-            Pattern.MULTILINE
-    );
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static final String TRANSLATE_URL = "https://translate-pa.googleapis.com/v1/translate?" +
+            "params.client=gtx&" +
+            "dataTypes=TRANSLATION&" +
+            "key=AIzaSyDLEeFI5OtFBwYBIoK_jj5m32rZK5CkCXA&" + // some google API key :eyes:
+            "query.sourceLanguage=%s&" +
+            "query.targetLanguage=%s&" +
+            "query.text=%s";
 
     public TranslateCommand () {
         super(
@@ -47,30 +51,20 @@ public class TranslateCommand extends Command {
             try {
                 final URL url = new URI(
                         String.format(
-                                TRANSLATE_MOBILE_URL,
+                                TRANSLATE_URL,
                                 URLEncoder.encode(from, StandardCharsets.UTF_8),
                                 URLEncoder.encode(to, StandardCharsets.UTF_8),
                                 URLEncoder.encode(message, StandardCharsets.UTF_8)
                         )
                 ).toURL();
 
-                final String html = HttpUtilities.getRequest(url);
-
-                final Matcher matcher = FIND_RESULT_PATTERN.matcher(html);
-
-                if (!matcher.find()) return;
-
-                final String match = matcher.group(1);
-                if (match == null) return;
-
-                final String result = HTMLUtilities.toFormattingCodes(match);
-                if (result == null) return;
+                final Result result = OBJECT_MAPPER.readValue(url, Result.class);
 
                 context.sendOutput(
                         Component
                                 .translatable(
                                         "Result: %s",
-                                        Component.text(result).color(NamedTextColor.GREEN)
+                                        Component.text(result.translation()).color(NamedTextColor.GREEN)
                                 )
                                 .color(bot.colorPalette.secondary)
                 );
@@ -80,5 +74,31 @@ public class TranslateCommand extends Command {
         });
 
         return null;
+    }
+
+    /*
+        $ curl "https://translate-pa.googleapis.com/v1/translate?params.client=gtx&dataTypes=TRANSLATION&key=AIzaSyDLEeFI5OtFBwYBIoK_jj5m32rZK5CkCXA&query.sourceLanguage=auto&query.targetLanguage=ja&query.text=hello"
+        {
+          "translation": "こんにちは",
+          "detectedLanguages": {
+            "srclangs": [
+              "en"
+            ],
+            "extendedSrclangs": [
+              "en"
+            ]
+          },
+          "sourceLanguage": "en"
+        }
+     */
+    private record Result(
+            @JsonValue String translation,
+            @JsonValue String sourceLanguage,
+            @JsonValue DetectedLanguages detectedLanguages
+    ) {
+        private record DetectedLanguages(
+                @JsonValue List<String> srclangs,
+                @JsonValue List<String> extendedSrclangs
+        ) { }
     }
 }
