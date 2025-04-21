f1d24b8a59f5be07c032002a1f7dcc6c2896ad1c
ChomeNS <95471003+ChomeNS@users.noreply.github.com>
2025-04-21 20:23:43 +0700

    fix: AAAA I COMMIT THE TEST CODE

--- src/main/java/me/chayapak1/chomens_bot/commands/TestCommand.java
+++ src/main/java/me/chayapak1/chomens_bot/commands/TestCommand.java
@@ -5,6 +5,7 @@ import me.chayapak1.chomens_bot.command.CommandContext;
 import me.chayapak1.chomens_bot.command.CommandException;
 import me.chayapak1.chomens_bot.command.TrustLevel;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 
 public class TestCommand extends Command {
     public TestCommand () {
@@ -19,17 +20,13 @@ public class TestCommand extends Command {
 
     @Override
     public Component execute (final CommandContext context) throws CommandException {
-        for (final Thread thread : Thread.getAllStackTraces().keySet()) {
-            System.out.println(thread);
-        }
-        return null;
-//        return Component.translatable(
-//                "Hello, World! Username: %s, Sender UUID: %s, Prefix: %s, Flags: %s, Args: %s",
-//                Component.text(context.sender.profile.getName()),
-//                Component.text(context.sender.profile.getIdAsString()),
-//                Component.text(context.prefix),
-//                Component.text(String.join(" ", context.getFlags())),
-//                Component.text(context.getString(true, false))
-//        ).color(NamedTextColor.GREEN);
+        return Component.translatable(
+                "Hello, World! Username: %s, Sender UUID: %s, Prefix: %s, Flags: %s, Args: %s",
+                Component.text(context.sender.profile.getName()),
+                Component.text(context.sender.profile.getIdAsString()),
+                Component.text(context.prefix),
+                Component.text(String.join(" ", context.getFlags())),
+                Component.text(context.getString(true, false))
+        ).color(NamedTextColor.GREEN);
     }
 }
