c68e300047ae7d96a8b6ddc44e99ee109ea4da6c
ChomeNS <95471003+ChomeNS@users.noreply.github.com>
2025-05-25 07:53:52 +0700

    feat: add more stuff to AuthCommand but restrict setting other player's level to `TrustLevel.MAX` only

--- src/main/java/me/chayapak1/chomens_bot/commands/AuthCommand.java
+++ src/main/java/me/chayapak1/chomens_bot/commands/AuthCommand.java
@@ -5,13 +5,14 @@ import me.chayapak1.chomens_bot.command.Command;
 import me.chayapak1.chomens_bot.command.CommandContext;
 import me.chayapak1.chomens_bot.command.CommandException;
 import me.chayapak1.chomens_bot.command.TrustLevel;
+import me.chayapak1.chomens_bot.data.player.PlayerEntry;
 import net.kyori.adventure.text.Component;
 
 public class AuthCommand extends Command {
     public AuthCommand () {
         super(
                 "auth",
-                new String[] {},
+                new String[] { "[TrustLevel]" },
                 new String[] {},
                 TrustLevel.TRUSTED
         );
@@ -19,16 +20,40 @@ public class AuthCommand extends Command {
 
     @Override
     public Component execute (final CommandContext context) throws CommandException {
-        context.checkOverloadArgs(0);
+        final boolean allowSettingOthers = context.trustLevel == TrustLevel.MAX;
+
+        if (!allowSettingOthers) context.checkOverloadArgs(1);
 
         final Bot bot = context.bot;
 
-        context.sender.authenticatedTrustLevel = context.trustLevel;
+        TrustLevel trustLevel = context.getEnum(false, TrustLevel.class);
+        if (trustLevel == null) trustLevel = context.trustLevel;
 
-        return Component.translatable(
-                "commands.auth.output",
-                bot.colorPalette.defaultColor,
-                context.sender.authenticatedTrustLevel.component
+        if (trustLevel.level > context.trustLevel.level) throw new CommandException(
+                Component.translatable("commands.auth.error.privilege_escalate")
         );
+
+        final String targetString = context.getString(true, false);
+
+        PlayerEntry target = null;
+        if (allowSettingOthers && !targetString.isEmpty()) target = bot.players.getEntry(targetString);
+        if (target == null) target = context.sender;
+
+        target.authenticatedTrustLevel = trustLevel;
+
+        if (target.equals(context.sender)) {
+            return Component.translatable(
+                    "commands.auth.self",
+                    bot.colorPalette.defaultColor,
+                    target.authenticatedTrustLevel.component
+            );
+        } else {
+            return Component.translatable(
+                    "commands.auth.others",
+                    bot.colorPalette.defaultColor,
+                    Component.text(target.profile.getName(), bot.colorPalette.username),
+                    target.authenticatedTrustLevel.component
+            );
+        }
     }
 }
