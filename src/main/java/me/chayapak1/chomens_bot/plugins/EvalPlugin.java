50d740f927d297d571ccd0cc06d5f1c833319038
ChomeNS <95471003+ChomeNS@users.noreply.github.com>
2025-08-11 14:28:55 +0700

    fix: eval bridge dupe bug when eval server restarts + use a separate executor service with queue check

--- src/main/java/me/chayapak1/chomens_bot/plugins/EvalPlugin.java
+++ src/main/java/me/chayapak1/chomens_bot/plugins/EvalPlugin.java
@@ -1,5 +1,6 @@
 package me.chayapak1.chomens_bot.plugins;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
@@ -17,9 +18,20 @@ import me.chayapak1.chomens_bot.util.LoggerUtilities;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class EvalPlugin {
+    // we need another executor service to not interfere with the main one (in case people spam the bridge)
+    private static final ExecutorService EXECUTOR_SERVICE = Executors.newFixedThreadPool(
+            2,
+            new ThreadFactoryBuilder()
+                    .setNameFormat("ExecutorService (eval)")
+                    .build()
+    );
+
     private static final String BRIDGE_PREFIX = "function:";
     public static final List<EvalFunction> FUNCTIONS = ObjectList.of(
             new CoreFunction(),
@@ -32,6 +44,7 @@ public class EvalPlugin {
 
     private static Socket socket = null;
     public static boolean connected = false;
+    private static boolean initialized = false;
 
     private static final AtomicInteger transactionId = new AtomicInteger();
     private static final Map<Integer, CompletableFuture<EvalOutput>> futures = new Object2ObjectOpenHashMap<>();
@@ -44,58 +57,66 @@ public class EvalPlugin {
             return;
         }
 
+        final JsonArray functionsArray = new JsonArray();
+
+        for (final Bot bot : Main.bots) {
+            final String server = bot.getServerString(true);
+
+            for (final EvalFunction function : FUNCTIONS) {
+                final JsonObject object = new JsonObject();
+
+                object.addProperty("name", function.name);
+                object.addProperty("server", server);
+
+                functionsArray.add(object);
+            }
+        }
+
+        socket.on("codeOutput", outputArgs -> {
+            if (outputArgs.length < 3) return;
+
+            try {
+                final int id = (int) outputArgs[0];
+                final boolean isError = (boolean) outputArgs[1];
+                final String output = (String) outputArgs[2];
+
+                if (!futures.containsKey(id)) return;
+
+                final CompletableFuture<EvalOutput> future = futures.remove(id);
+
+                future.complete(new EvalOutput(isError, output));
+            } catch (final ClassCastException | NumberFormatException ignored) { }
+        });
+
         socket.on(Socket.EVENT_CONNECT, args -> {
             connected = true;
 
-            final JsonArray functionsArray = new JsonArray();
+            socket.emit("setFunctions", GSON.toJson(functionsArray));
+
+            if (initialized) return;
+            else initialized = true;
 
             for (final Bot bot : Main.bots) {
-                final String server = bot.getServerString(true);
-
-                for (final EvalFunction function : FUNCTIONS) {
-                    final JsonObject object = new JsonObject();
-
-                    object.addProperty("name", function.name);
-                    object.addProperty("server", server);
-
-                    functionsArray.add(object);
-                }
-
                 for (final EvalFunction function : FUNCTIONS) {
                     socket.on(
                             BRIDGE_PREFIX + function.name + ":" + bot.getServerString(true),
-                            functionArgs ->
-                                    Main.EXECUTOR_SERVICE.execute(() -> {
-                                        try {
-                                            final EvalFunction.Output output = function.execute(bot, functionArgs);
-                                            if (output == null) return;
-                                            socket.emit("functionOutput:" + function.name, output.message(), output.parseJSON());
-                                        } catch (final Exception ignored) { }
-                                    })
+                            functionArgs -> {
+                                final ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) EXECUTOR_SERVICE;
+
+                                // ignore the bridge request from the eval server if we have too much
+                                if (threadPoolExecutor.getQueue().size() > 75) return;
+
+                                EXECUTOR_SERVICE.execute(() -> {
+                                    try {
+                                        final EvalFunction.Output output = function.execute(bot, functionArgs);
+                                        if (output == null) return;
+                                        socket.emit("functionOutput:" + function.name, output.message(), output.parseJSON());
+                                    } catch (final Exception ignored) { }
+                                });
+                            }
                     );
                 }
-
-                socket.on("codeOutput", outputArgs -> {
-                    if (outputArgs.length < 3) return;
-
-                    try {
-                        final int id = (int) outputArgs[0];
-                        final boolean isError = (boolean) outputArgs[1];
-                        final String output = (String) outputArgs[2];
-
-                        if (!futures.containsKey(id)) return;
-
-                        final CompletableFuture<EvalOutput> future = futures.remove(id);
-
-                        future.complete(new EvalOutput(isError, output));
-                    } catch (final ClassCastException | NumberFormatException ignored) { }
-                });
             }
-
-            socket.emit(
-                    "setFunctions",
-                    GSON.toJson(functionsArray)
-            );
         });
         socket.on(Socket.EVENT_DISCONNECT, args -> connected = false);
         socket.on(Socket.EVENT_CONNECT_ERROR, args -> connected = false);
